d59b65f3da5619e1591dbd26b406fd31c7b418f0
Zakarum <zakarumych@ya.ru>
2020-11-13 20:11:41 +0300

    Add erupt example.
    Copy examples to gfx-alloc-{gfx,erupt} docs

---
+++ examples/src/erupt.rs
@@ -0,0 +1,72 @@
+use {
+    erupt::{vk1_0, DefaultEntryLoader, DeviceLoader, InstanceLoader},
+    gpu_alloc::{Config, GpuAllocator, Request, UsageFlags},
+    gpu_alloc_erupt::{device_properties, EruptMemoryDevice},
+    std::ffi::CStr,
+};
+
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+
+    let entry = DefaultEntryLoader::new()?;
+
+    let instance = InstanceLoader::new(
+        &entry,
+        &vk1_0::InstanceCreateInfo::default()
+            .into_builder()
+            .application_info(
+                &vk1_0::ApplicationInfo::default()
+                    .into_builder()
+                    .engine_name(CStr::from_bytes_with_nul(b"GpuAlloc\0").unwrap())
+                    .engine_version(1)
+                    .application_name(CStr::from_bytes_with_nul(b"GpuAllocApp\0").unwrap())
+                    .application_version(1)
+                    .api_version(entry.instance_version()),
+            ),
+        None,
+    )?;
+
+    let physical_devices = unsafe { instance.enumerate_physical_devices(None) }.result()?;
+    let physical_device = physical_devices[0];
+
+    let props = unsafe { device_properties(&instance, physical_device) }?;
+
+    let device = DeviceLoader::new(
+        &instance,
+        physical_device,
+        &vk1_0::DeviceCreateInfoBuilder::new().queue_create_infos(&[
+            vk1_0::DeviceQueueCreateInfoBuilder::new()
+                .queue_family_index(0)
+                .queue_priorities(&[0f32]),
+        ]),
+        None,
+    )?;
+
+    let config = Config::i_am_potato();
+
+    let mut allocator = GpuAllocator::new(config, props);
+
+    let mut block = unsafe {
+        allocator.alloc(
+            EruptMemoryDevice::wrap(&device),
+            Request {
+                size: 10,
+                align_mask: 1,
+                usage: UsageFlags::HOST_ACCESS,
+                memory_types: !0,
+            },
+        )
+    }?;
+
+    unsafe {
+        block.write_bytes(
+            EruptMemoryDevice::wrap(&device),
+            0,
+            &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        )
+    }?;
+
+    unsafe { allocator.dealloc(EruptMemoryDevice::wrap(&device), block) }
+
+    Ok(())
+}
--- gfx/src/lib.rs
+++ gfx/src/lib.rs
@@ -1,3 +1,96 @@
+//!
+//! # Gfx backend for `gpu-alloc`
+//!  
+//! # Usage example
+//!
+//! ```ignore
+//!
+//! use {
+//!     eyre::eyre,
+//!     gfx_backend_vulkan::Instance,
+//!     gfx_hal::{
+//!         adapter::{Gpu, PhysicalDevice as _},
+//!         queue::QueueFamily as _,
+//!         Features, Instance as _,
+//!     },
+//!     gpu_alloc::{Config, GpuAllocator, Request, UsageFlags},
+//!     gpu_alloc_gfx::{gfx_device_properties, GfxMemoryDevice},
+//! };
+//!
+//! fn main() -> eyre::Result<()> {
+//!     color_eyre::install()?;
+//!
+//!     let instance =
+//!         Instance::create("gpu_alloc-example", 1).map_err(|_| eyre!("Unsupported backend"))?;
+//!
+//!     let adapters = instance.enumerate_adapters();
+//!
+//!     let adapter = adapters
+//!         .iter()
+//!         .min_by_key(|a| {
+//!             use gfx_hal::adapter::DeviceType::*;
+//!             match a.info.device_type {
+//!                 Other => 4,
+//!                 IntegratedGpu => 1,
+//!                 DiscreteGpu => 0,
+//!                 VirtualGpu => 2,
+//!                 Cpu => 3,
+//!             }
+//!         })
+//!         .ok_or_else(|| eyre!("No adapters found"))?;
+//!
+//!     let queue_family = adapter
+//!         .queue_families
+//!         .iter()
+//!         .min_by_key(|qf| {
+//!             use gfx_hal::queue::QueueType::*;
+//!             match qf.queue_type() {
+//!                 General => 0,
+//!                 Graphics => 1,
+//!                 Compute => 3,
+//!                 Transfer => 4,
+//!             }
+//!         })
+//!         .ok_or_else(|| eyre!("No queue families found"))?;
+//!
+//!     let props = gfx_device_properties(adapter);
+//!
+//!     let Gpu { device, .. } = unsafe {
+//!         adapter
+//!             .physical_device
+//!             .open(&[(queue_family, &[1.0])], Features::empty())
+//!     }?;
+//!
+//!     let config = Config::i_am_potato();
+//!
+//!     let mut allocator = GpuAllocator::new(config, props);
+//!
+//!     let mut block = unsafe {
+//!         allocator.alloc(
+//!             GfxMemoryDevice::wrap(&device),
+//!             Request {
+//!                 size: 10,
+//!                 align_mask: 1,
+//!                 usage: UsageFlags::HOST_ACCESS,
+//!                 memory_types: !0,
+//!             },
+//!         )
+//!     }?;
+//!
+//!     unsafe {
+//!         block.write_bytes(
+//!             GfxMemoryDevice::wrap(&device),
+//!             0,
+//!             &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+//!         )
+//!     }?;
+//!
+//!     unsafe { allocator.dealloc(GfxMemoryDevice::wrap(&device), block) }
+//!
+//!     Ok(())
+//! }
+//!
+
 use {
     gfx_hal::{
         adapter::{Adapter, PhysicalDevice as _},
